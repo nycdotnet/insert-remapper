05e3632e720a1f6553b24d92bf94ae86d7a938d1
Steve Ognibene <steve.ognibene@outlook.com>
2014-11-25 22:10:31 -0500

    include gruntfile.js

---
+++ insert-remapper/gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        ts: {
+            options: {
+                target: 'es5',
+                module: 'commonjs',
+                noImplicitAny: true,
+                fast: "never",
+                compiler: './node_modules/grunt-ts/customcompiler/tsc'
+            },
+            dev: {
+                src: ["*.ts"]
+            }
+        },
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.registerTask("default", ["ts:dev"]);
+};
+
