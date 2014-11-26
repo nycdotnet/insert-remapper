506ebd746ee601f69193d03e4edab94f03f730ac
Steve Ognibene <steve.ognibene@outlook.com>
2014-11-25 22:06:56 -0500

    remove unneeded server file

--- insert-remapper/server.js
+++
@@ -1,4 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname, { 'index': ['index.html'] })).listen(8080);
-//# sourceMappingURL=server.js.map
--- insert-remapper/server.ts
+++
@@ -1,4 +0,0 @@
-﻿//Thanks: http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname, { 'index': ['index.html'] })).listen(8080);
