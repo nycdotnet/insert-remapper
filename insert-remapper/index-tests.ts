62f716469cdf4ba13065659c80b4a21abe9b1030
Steve Ognibene <steve.ognibene@outlook.com>
2014-11-25 23:34:38 -0500

    added qUnit

---
+++ insert-remapper/index-tests.ts
@@ -0,0 +1,14 @@
+﻿///<reference path="typings/tsd.d.ts" />
+
+QUnit.module("index-tests");
+
+import InsertRemapper = require("index");
+
+
+test("Can create the insertRemapper", 1, function () {
+    var remapper = new InsertRemapper("", "", "", "");
+    ok(true, "a dummy");
+});
+
+
+
---
+++ insert-remapper/tests.ts
@@ -0,0 +1,21 @@
+﻿///<reference path="typings/tsd.d.ts" />
+
+interface qUnitTestRunner {
+    run: (files: any, callback?: ()=> void) => void;
+}
+
+import fs = require('fs');
+import insertRemapper = require('index');
+var testrunner : qUnitTestRunner = require('qunit');
+
+var testsComplete = function () {
+    console.log('Tests completed...');
+}
+
+console.log('Tests beginning...');
+
+testrunner.run({
+    code: "index.js",
+    tests: "index-tests.js"
+}, testsComplete);
+
