6bcdc216782c20e825c074b5c85d0863cf3da795
Steve Ognibene <steve.ognibene@outlook.com>
2014-11-26 10:07:57 -0500

    basic test working

--- insert-remapper/index-tests.ts
+++ insert-remapper/index-tests.ts
@@ -4,12 +4,9 @@ QUnit.module("index-tests");
 
 import InsertRemapper = require("index"); ///ts:doNotImport 
 
-
-
-
-test("Can create the insertRemapper", 1, function () {
-    //var remapper = new InsertRemapper("", "", "", "");
-    ok(true, "a dummy");
+test("Can create the InsertRemapper", 1, function () {
+    var remapper = new InsertRemapper("", "", "", "");
+    ok(true, "pass if no crash");
 });
 
 
