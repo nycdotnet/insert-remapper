f4b0004a3ab2b4ad75a205ac4ae12c23bd82f718
Steve Ognibene <steve.ognibene@outlook.com>
2014-11-28 22:26:56 -0500

    removed commented code from gruntfile

--- insert-remapper/gruntfile.js
+++ insert-remapper/gruntfile.js
@@ -17,40 +17,10 @@
         nodeunit: {
             dev: ['tests/index-nodeunit-tests.js']
         }
-        //qunit: {
-        //    dev: {
-        //        options: {
-        //            urls: [
-        //              'http://localhost:8000/tests/index.html'
-        //            ]
-        //        }
-        //    }
-        //},
-        //connect: {
-        //    dev: {
-        //        options: {
-        //            port: 8000,
-        //            base: '.',
-        //            hostname: '*'
-        //        }
-        //    }
-        //},
-        //browserify: {
-        //    dev: {
-        //        files : {
-        //            'dist/index.js' : 'index.js',
-        //            'dist/tests/index-tests.js': 'tests/index-tests.js'
-        //        }
-        //    }
-        //}
     });
 
     grunt.loadNpmTasks("grunt-ts");
-    //grunt.loadNpmTasks('grunt-contrib-qunit');
-    //grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    //grunt.loadNpmTasks('grunt-browserify');
-    //grunt.registerTask("default", ["ts", "browserify", "connect", "qunit"]);
     grunt.registerTask("default", ["ts", "nodeunit"]);
 };
 
